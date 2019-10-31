7e705a4c578b21a2e9e163a7646f75fb6d137154
alcabilone <alcabilone@gmail.com>
2019-10-31 20:29:35 +0200

    added password variable and new Constructor

--- Employee.java
+++ Employee.java
@@ -5,17 +5,22 @@ public abstract class Employee {
 	private String nameSurname;
 	private String telephone;
 	private String email;
+	private String password;
 	private int id;
 	public Random rdm = new Random();
 	public ArrayList<Employee> employees = new ArrayList<Employee>();
 
-	public Employee(String nameSurname , String telephone, String email, Department department) {
+	public Employee(String nameSurname ,String password, String telephone, String email, Department department) {
 		this.nameSurname = nameSurname;
+		this.password = password;
 		this.telephone = telephone;
 		this.email = email;
 		this.department = department;
 		this.id=rdm.nextInt(10000);
 		employees.add(this);
+	}
+	public Employee(String nameSurname ,String password, Department department) {
+		super(nameSurname,password,null.null.department);
 	}
 	public String getDepartment() {
 		return department;
@@ -32,6 +37,9 @@ public abstract class Employee {
 	public int getId() {
 		return id;
 	}
+	public String getPassword() {
+		return password;
+	}
 	public abstract String getJobTitle();
 
 	public void setDepartment(Department department) {
@@ -45,6 +53,9 @@ public abstract class Employee {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	@Override
 	public String toString() {

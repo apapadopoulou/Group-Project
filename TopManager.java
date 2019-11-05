980228c22f4307a5fdc045a81eedad853998ada0
Natalia Tsamtsuri <t8180138@aueb.gr>
2019-11-05 21:02:21 +0200

    I removed symbol _ from TopManager's constructor

--- TopManager.java
+++ TopManager.java
@@ -1,6 +1,6 @@
 public class TopManager extends Employee{
-	private ArrayList <MiddleManager> managingManagers = new ArrayList <MiddleManager>();
-	public Top_Manager( String name, String surname, String password, String telephone, String email, Department department){
-		super(name, surname, password, telephone, email, department);
-	}
+  private ArrayList <MiddleManager> managingManagers = new ArrayList <MiddleManager>();
+  public TopManager( String name, String surname, String password, String telephone, String email, Department department){
+    super(name, surname, password, telephone, email, department);
+  }
 }

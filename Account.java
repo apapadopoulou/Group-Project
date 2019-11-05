c039b4afc4c15d9f34f3ac1a69a06a62b4b9e866
panos papasotiriou <panos1606@gmail.com>
2019-11-06 01:11:21 +0200

    moving lines from checkers to account

--- Account.java
+++ Account.java
@@ -14,5 +14,26 @@ public class Account {
 	 email = e.getEmail();
 	 password = e.getPassword();
 	 id = e.getId();
+	 /*
+	 if (isValidEmail(email)) 
+         System.out.print("Valid Email"); 
+     else
+         System.out.print("Invalid Email"); 
+     	System.out.print("Please retype your Email");	
+     if (isValidFirstName(firstName)) 
+         System.out.print("Valid First Name"); 
+     else
+         System.out.print("Invalid First Name");
+     	System.out.print("Please retype your First Name");
+     if (isValidLastName(lastName)) 
+         System.out.print("Valid Last Name"); 
+     else
+         System.out.print("Invalid Last Name");
+    		System.out.print("Please retype your Last Name");
+     if (isValidPhoneNumber(phoneNo)) 
+         System.out.print("Valid Phone Number"); 
+     else
+         System.out.print("Invalid Phone number");
+     	System.out.print("Please retype your Phone Number");
+ */ }  
  }
-}
--- Checkers.java
+++ Checkers.java
@@ -32,34 +32,5 @@ public class Checkers {
 			else return false;
 	   }
 
-	// for class Account
-		   		public static void main(String[] args) 
-		    { 
-		        //input variables
-				String email = "panos1606@gmail.com"; 
-		        String firstName = "Panos";
-		        String lastName = "Papasotiriou";		
-		        String phoneNo = "2109613432";
-		        if (isValidEmail(email)) 
-		            System.out.print("Valid Email"); 
-		        else
-		            System.out.print("Invalid Email"); 
-		        	System.out.print("Please retype your Email");	
-		        if (isValidFirstName(firstName)) 
-		            System.out.print("Valid First Name"); 
-		        else
-		            System.out.print("Invalid First Name");
-		        	System.out.print("Please retype your First Name");
-		        if (isValidLastName(lastName)) 
-		            System.out.print("Valid Last Name"); 
-		        else
-		            System.out.print("Invalid Last Name");
-		       		System.out.print("Please retype your Last Name");
-		        if (isValidPhoneNumber(phoneNo)) 
-		            System.out.print("Valid Phone Number"); 
-		        else
-		            System.out.print("Invalid Phone number");
-		        	System.out.print("Please retype your Phone Number");
-		    } 
-	}  
+
 	

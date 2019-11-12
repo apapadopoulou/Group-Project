66757848ebd064f9bf13bccb2bb2ee696544d327
alcabilone <alcabilone@gmail.com>
2019-11-12 11:49:48 +0200

    Fixed some things

--- PersonalisedCalendar.java
+++ PersonalisedCalendar.java
@@ -4,21 +4,21 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.ArrayList;
 
-public class PersonalisedCalendar extends Calendar {
-	private Day [] calendar = new Day [12];
+public class PersonalisedCalendar /*extends Calendar*/ {
+	private Day [] [] calendar = new Day [12][];
 	public PersonalisedCalendar() {
       for (int month = 0; month <= 11; month++) {
 	  			if (month <= 6 & month != 1){
 	  			  if (month%2 == 1) {
-	  				calendar[month] = new Day [31];
-	  			  } else if (month%2 = 0) {
+	  				calendar[month] =new  Day [31];
+	  			  } else if (month%2 == 0) {
 	  				calendar[month] = new Day [30];
 	  			 } else if (month == 1){
 	  				 calendar[month] = new Day [29];
 	  			 } else {
 	  			    if (month%2 == 1) {
 	  						calendar[month] = new Day [30];
-	  				} else if (month%2 = 0) {
+	  				} else if (month%2 == 0) {
 	  						calendar[month] = new Day [31];
 	  				}
 	  			 }
@@ -30,10 +30,11 @@ public class PersonalisedCalendar extends Calendar {
 							JFrame f = new JFrame("Month");
 							f.setBounds(month, calendar[month].length, 4, 5);
 							f.setVisible(true);
+						}
 				}
 		public static void main (String [] args) {
 				PersonalisedCalendar c = new PersonalisedCalendar();
-				c.displayCalendar;
+				c.displayCalendar();
 
 		}
 	 }

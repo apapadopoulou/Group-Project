6094f7bf5f535544ccd4e785ee962d61ade3505a
Angela Papadopoulou <angelikeepap@gmail.com>
2019-11-14 17:39:54 +0200

    Added date formatter

--- Day.java
+++ Day.java
@@ -2,8 +2,11 @@ import java.util.ArrayList;
 public class Day {
   private ArrayList <Program> dailyProgram = new ArrayList <Program>();
   public Day() {
-	  
+	  LocalDate date = LocalDate.now();
+
+
   }
-  
-  
+  public void displayDay(LocalDate date) {
+	  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyy");
+  	  System.out.println(date.format(formatter));
 }

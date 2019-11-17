362ceb2099dafda662e37fb8d61630dbcadf6983
Katerina Divini <k_divini@yahoo.gr>
2019-11-17 21:35:05 +0200

    i created the class event

---
+++ Event.java
@@ -0,0 +1,33 @@
+
+public class Event {
+	private String title;
+	private String date;
+
+	public Event(String title, String date) {
+		this.title = title;
+		do {
+		if (Checkers.isValidTitle(title)) 
+			System.out.println("Valid Title");
+		else 
+			System.out.println("Invalid Title; Insert a new one");
+		} while (Checkers.isValidTitle(title));
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}

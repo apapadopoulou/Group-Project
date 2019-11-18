eb5db026a5c847fc21f3907eb1f1cf1af633097a
Katerina Divini <k_divini@yahoo.gr>
2019-11-18 02:07:51 +0200

    added a method to validate the event

--- Checkers.java
+++ Checkers.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-import java.util.regex.Pattern; 
+import java.util.regex.Pattern;
 
 public class Checkers {
 	// validate email
-		public static boolean isValidEmail(String email) 
-		{ 
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
-                            "[a-zA-Z0-9_+&*-]+)*@" + 
-                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
-                            "A-Z]{2,7}$"; 
-                              
-        Pattern pat = Pattern.compile(emailRegex); 
-        if (email == null) 
-            return false; 
-        return pat.matcher(email).matches(); 
-    } 
+		public static boolean isValidEmail(String email)
+		{
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
 		// validate last name
 		public static boolean isValidFirstName( String firstName ) {
 	      return firstName.matches( "[A-Z][a-z]*" );
@@ -23,16 +23,16 @@ public class Checkers {
 	   public static boolean isValidLastName( String lastName ) {
 	      return lastName.matches( "[A-Z][a-z]*" );
 	   }
-	
+
 	   // validate phone number
 	   private static boolean isValidPhoneNumber(String phoneNo) {
 			//validate phone numbers of format "1234567890"
-			if (phoneNo.matches("\\d{10}")) 
+			if (phoneNo.matches("\\d{10}"))
 				return true;
 			//validating phone number with -, . or spaces
 			else if(phoneNo.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"))
 				return true;
-			else 
+			else
 				return false;
 	   }
 			public static String ValidPassword() {
@@ -41,15 +41,15 @@ public class Checkers {
 			      Scanner input = new Scanner(System.in);
 			      System.out.printf("Please create your password:\n");
 			      password = input.nextLine();
-			      
+
 			      //Checking password length
 			      while( (password.length() < 6) || (password.length() > 15) )
 			      {
 			         System.out.print("This password must be between 6 and 15 characters. Try again: ");
 			         password = input.nextLine();
 			      }
-			      //Confirming if passwords match   
-			      String password2;   
+			      //Confirming if passwords match
+			      String password2;
 			      System.out.print("\nPlease type your password again to confirm: ");
 			      password2 = input.nextLine();
 
@@ -61,7 +61,10 @@ public class Checkers {
 			      return password;
 
 			   }
+			   //vallidate event title
+			   public static boolean isValidTitle(String title) {
+				   return title == "appointment" || title == "meeting" || title == "main event";
+			   }
 	   }
 
 
-	

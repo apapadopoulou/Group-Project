45a66d4a44fa28181b4fcfcb0349c92fb0836e21
Sarantos Moraitis <praetorians12345@gmail.com>
2019-11-27 23:26:59 +0200

    Added comments for this class

--- HumanResources.java
+++ HumanResources.java
@@ -1,12 +1,13 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class HumanResources {
 	private String email;
 	private double salary;
 	private String name;
 	private String surname;
-	public static ArrayList<HumanResources> employees = new ArrayList<HumanResources>();
-
-	public HumanResources(String email, double salary, String name, String surname) {
+	public static ArrayList <HumanResources> employees = new ArrayList<HumanResources>();
+    /*Constructor for class HumanResources*/
+    public HumanResources(String email, double salary, String name, String surname) {
 		this.email = email;
 		this.salary = salary;
 		this.name = name;
@@ -38,6 +39,7 @@ public class HumanResources {
     public double getSalary() {
 		return salary;
     }
+/*Using for loop in order to find out if the employee is a HR Director and to display the salary of the current employee*/
        for (counter = 0; counter <= employees.size(); counter++) {
         if (email.contains("HR")) {
             System.out.println("The Keyword :HR: is found. The connected user is a HR Director!");
@@ -50,14 +52,15 @@ public class HumanResources {
 
 
 
-
+/*Creating method to display the menu to the employee*/
    public static void Display_Menu() {
 	   System.out.println("1) Edit Salaries\n 2) Edit Timeoffs \n 3) See Calendar");
 	   System.out.print( "Selection:");
    }
+   /*Creating method Menu1 to make menu operating*/
    public static void Menu1() {
 	   Scanner sc = new Scanner(System.in);
-	   Display_Menu();
+	   Display_Menu(); /*Calling method Display_Menu*/
 	   switch (sc.nextInt()) {
 		   case 1:
 		     System.out.println("You picked Edit Salaries");
@@ -73,6 +76,6 @@ public class HumanResources {
 	   }
    }
    public static void main(String[] args) {
-	   Menu1();
+	   Menu1(); /*Calling method Menu1*/
    }
 }

3e0571affd47a79142e7707a9e46a2ca2903f496
Natalia Tsamtsuri <t8180138@aueb.gr>
2019-12-02 13:54:36 +0200

    added an Arraylist in BasicEmployee

--- BasicEmployee.java
+++ BasicEmployee.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
 public class BasicEmployee extends Employee {
-	public BasicEmployee(String name, String surname,int telephone, String email, Department department, String id) {
-		super(name, surname, telephone, email, department);
-	}
-
-	@Override
-	public String getJobTitle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  private ArrayList <BasicEmployee> basicemployees = new ArrayList <BasicEmployee>();
+  public BasicEmployee(String name, String surname,int telephone, String email, Department department, String id) {
+    super(name, surname, telephone, email, department);
+    id = String.valueOf(department.getId()) + name.substring(1,2) + surname.substring(1,2) + String.valueOf(department.getEmployeesOfDepartment().size());
+    employees.add(this);
+    basicemployees.add(this);
+  }
+  
+  @Override
+  public String getJobTitle() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 }

8b559be75de4cfc7d32507b077c43ee2c773009e
panos papasotiriou <panos1606@gmail.com>
2019-12-03 00:27:06 +0200

    Adding one object with day task and event in per calendar

--- PersonalisedCalendar.java
+++ PersonalisedCalendar.java
@@ -5,6 +5,15 @@ import java.util.GregorianCalendar;
 import java.util.ArrayList;
 
 public class PersonalisedCalendar /*extends Calendar*/ {
+	private Day day;
+	private Task task;
+	private Event event;
+	/*Creating constructor */
+	public PersonalisedCalendar(Day day, Task task, Event event) {
+		this.day = day;
+		this.task = task;
+		this.event = event;
+	}
 	/* Creating a 2X2 table named calendar of type Day and containing every day of the year*/
 	private Day [] [] calendar = new Day [12][]; 
 	/* PersonalisedCalendar method inputs the specific amount of days each month has */
@@ -35,13 +44,23 @@ public class PersonalisedCalendar /*extends Calendar*/ {
 							f.setBounds(month, calendar[month].length, 4, 5);
 							f.setVisible(true);
 						}
-				}
+						 /* This way connect three classes (Day,Task,Event) in order to display the calendar */
+						Scanner p = new Scanner(System.in);
+						int lev = p.nextInt();
+						String title = p.nextLine();
+						String date = p.nextLine();
+						Day d = new Day();
+						Task t = new Task(getDueDate() , lev);
+						Event e = new Event(title , date);
+						PersonalisedCalendar pc = new PersonalisedCalendar(d, t, e); 
+						
+		}
 		/* Main method that assigns a personalised calendar to a user and displays it */
 		public static void main (String [] args) {
 				PersonalisedCalendar c = new PersonalisedCalendar();
 				c.displayCalendar();
 
 		}
-	 }
+		}
 
 

a25fcec64ab9204b0e5e3ecb98f874f15481c4f5
Natalia Tsamtsuri <t8180138@aueb.gr>
2019-12-03 03:19:23 +0200

    converted telephne from int to string

--- TopManager.java
+++ TopManager.java
@@ -5,10 +5,10 @@ public class TopManager extends Employee{
   private ArrayList <MiddleManager> managingManagers = new ArrayList <MiddleManager>();
   private ArrayList <String> namesOfManagingManagers = new ArrayList <String>();
   public static ArrayList <TopManager> topManagers = new ArrayList <TopManager>();
-  public TopManager( String name, String surname, int telephone, String email, Department department){
+  public TopManager( String name, String surname, String telephone, String email, Department department){
     super(name, surname, telephone, email, department);
     for(int i = 1; i < MiddleManager.middleManagers.size(); i++) {
-    	MiddleManager.searchMiddleManagerById(String.valueOf(i)).toString();
+      MiddleManager.searchMiddleManagerById(String.valueOf(i)).toString();
     }
     System.out.println("Type the ids of the managers you are responsible for");
     System.out.println("Press enter after each id entry");
@@ -16,13 +16,13 @@ public class TopManager extends Employee{
     Scanner input = new Scanner(System.in);
     int md_id = input.nextInt();
     while (md_id != 0) {
-    	if (md_id > MiddleManager.middleManagers.size() || md_id < 0 ) {
-    		System.out.println("Wrong input. Please enter another id");
-    		md_id = input.nextInt();
-    		continue;
-      	}
-    	managingManagers.add(MiddleManager.searchMiddleManagerById(String.valueOf(md_id)));
-    	namesOfManagingManagers.add(MiddleManager.searchMiddleManagerById(String.valueOf(md_id)).getName());
+      if (md_id > MiddleManager.middleManagers.size() || md_id < 0 ) {
+        System.out.println("Wrong input. Please enter another id");
+        md_id = input.nextInt();
+        continue;
+        }
+      managingManagers.add(MiddleManager.searchMiddleManagerById(String.valueOf(md_id)));
+      namesOfManagingManagers.add(MiddleManager.searchMiddleManagerById(String.valueOf(md_id)).getName());
     }
     input.close();
     System.out.println("The managing managers have been saved");
@@ -31,13 +31,13 @@ public class TopManager extends Employee{
   
 @Override
 public String getJobTitle() {
-	// TODO Auto-generated method stub
-	return null;
+  // TODO Auto-generated method stub
+  return null;
 }
 public ArrayList <MiddleManager> getManagingManagers() {
-	return managingManagers;
+  return managingManagers;
 }
 public void setManagingManagers(ArrayList <MiddleManager> managingManagers) {
-	this.managingManagers = managingManagers;
+  this.managingManagers = managingManagers;
 }
 }

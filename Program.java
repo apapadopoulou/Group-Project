b758f40a8aa4b9ab369b58008197b0caad65e05e
Orestis Metaxas <metaxas99@gmail.com>
2019-12-04 13:37:34 +0200

    Created get and set methods for date

--- Program.java
+++ Program.java
@@ -1,4 +1,12 @@
 /*This class creates Event and Task objects.*/
 public abstract class Program {
-
+	Calendar calendar = new Calendar.getInstance();
+	private String date;
+	/*current_date = calendar.get(Calendar.DATE)+"/"+calendar.get(Calendar.MONTH)+"/"+calendar.get(Calendar.YEAR); na to tsekaroume an xreiazetai kapou*/
+	public void setDate(String date) {
+		this.date=date;
+	}
+	public String getDate() {
+		return date;
+	}
 }

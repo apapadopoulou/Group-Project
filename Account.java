d4d00b167b3c91ed3df53269a2283fa7c5e5a206
Katerina Divini <k_divini@yahoo.gr>
2019-12-09 01:44:03 +0200

    μορφοποιηση

--- Account.java
+++ Account.java
@@ -3,74 +3,78 @@ import javax.swing.JFrame;
 //import project.Employee;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
 import java.util.ArrayList;
+
 public class Account {
- private String department;
- private String name;
- private String surname;
- private int telephone;
- private String email;
- private String id;
- private String password;
- private static ArrayList<Account> accounts = new ArrayList<Account>();
+	private String department;
+ 	private String name;
+ 	private String surname;
+ 	private int telephone;
+ 	private String email;
+ 	private String id;
+ 	private String password;
+ 	private static ArrayList<Account> accounts = new ArrayList<Account>();
 
- /* Account's constructor */
+ 	/* Account's constructor */
 
- public Account(Employee e) {
-	 department = e.getDepartmentName();
-	 name = e.getName();
-	 surname = e.getSurname();
-	 telephone = e.getTelephone();
-	 email = e.getEmail();
-	 id = e.getId();
-	 password = Checkers.ValidPassword();
-	 accounts.add(this); /*Account object entered on accounts ArrayList*/
+ 	public Account(Employee e) {
+	 	department = e.getDepartmentName();
+	 	name = e.getName();
+	 	surname = e.getSurname();
+	 	telephone = e.getTelephone();
+	 	email = e.getEmail();
+	 	id = e.getId();
+	 	password = Checkers.ValidPassword();
+	 	accounts.add(this); /*Account object entered on accounts ArrayList*/
 
-	 /*
-	 if (isValidEmail(email))
-         System.out.print("Valid Email");
-     else
-         System.out.print("Invalid Email");
-         System.out.print("Please retype your Email");
-     if (isValidFirstName(firstName))
-         System.out.print("Valid First Name");
-     else
-         System.out.print("Invalid First Name");
-     	System.out.print("Please retype your First Name");
-     if (isValidLastName(lastName))
-         System.out.print("Valid Last Name");
-     else
-         System.out.print("Invalid Last Name");
+	 	/*
+	 	if (isValidEmail(email))
+         	System.out.print("Valid Email");
+     	else {
+         	System.out.print("Invalid Email");
+         	System.out.print("Please retype your Email");
+		}
+     	if (isValidFirstName(firstName))
+         	System.out.print("Valid First Name");
+     	else {
+         	System.out.print("Invalid First Name");
+     		System.out.print("Please retype your First Name");
+		}
+     	if (isValidLastName(lastName))
+         	System.out.print("Valid Last Name");
+     	else {
+         	System.out.print("Invalid Last Name");
     		System.out.print("Please retype your Last Name");
-     if (isValidPhoneNumber(phoneNo))
-         System.out.print("Valid Phone Number");
-     else
-         System.out.print("Invalid Phone number");
-     	System.out.print("Please retype your Phone Number");
- */
+		}
+     	if (isValidPhoneNumber(phoneNo))
+         	System.out.print("Valid Phone Number");
+     	else {
+         	System.out.print("Invalid Phone number");
+     		System.out.print("Please retype your Phone Number");
+		}
+ 		*/
 
- }
+	}
 
- /* This method searches Employees' accounts by their email*/
+ 	/* This method searches Employees' accounts by their email*/
 
- public Account searchAccountByEmail(String email) {
- 	boolean exists = false;
- 	for(int i=0;i<this.accounts.size();i++) {
- 		if (this.accounts.get(i).getEmail()==email) {
- 			exists=true;
- 			break;
- 		}
- 		if(exists==true) {
- 			return this.accounts;
- 		}else {
- 			System.out.println("The email you entered does not relate to an existing account");
- 			return null;
+ 	public Account searchAccountByEmail(String email) {
+ 		boolean exists = false;
+ 		for(int i=0;i<this.accounts.size();i++) {
+ 			if (this.accounts.get(i).getEmail()==email) {
+ 				exists=true;
+ 				break;
+ 			}
+ 			if(exists==true) {
+ 				return this.accounts;
+ 			}else {
+ 				System.out.println("The email you entered does not relate to an existing account");
+ 				return null;
+ 			}
  		}
  	}
- }
  	/*This method checks if the password and the email of the input
- 	 * are equal to the password and the email of the user.*/
+ 	   are equal to the password and the email of the user.*/
  	public static boolean login() {
  		boolean success=false;
  		//System.out.println("Welcome!");
@@ -98,7 +102,7 @@ public class Account {
  	//}
 
  	/* This method generates Employee's password */
- 	public static string passwordGenerator {
+ 	public static string passwordGenerator() {
 		PasswordGenerator gen = new PasswordGenerator();
 		CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
 		CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);

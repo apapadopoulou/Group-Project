ef4b6457e29ddbadc1829b01fdb768b3c405d021
Sarantos Moraitis <praetorians12345@gmail.com>
2019-12-08 20:36:05 +0200

    Added evaluation method and updated MiddleManager class

--- MiddleManager.java
+++ MiddleManager.java
@@ -60,10 +60,26 @@ public static MiddleManager searchMiddleManagerById(String id) {
 	      	 
 }
 
-public static Department showDepartments(Department departments) {
-	for (int i = 0; i < departments.size(); i++) {
-		System.out.println("The department name is:" + departments.get(i));
-		return departments.get(i);
+public static Department showDepartments(Department managingDepartments) {
+	for (int i = 0; i < managingDepartments.size(); i++) {
+		System.out.println("The department name is:" + managingDepartments.get(i));
+		return managingDepartments.get(i);
+	}
+}
+
+public static MiddleManager evaluation(int rating, MiddleManager middleManagers) {
+	for (int i = 0; i < middleManagers.size(); i++) {
+		if (rating == 1) {
+			System.out.println("The current employee needs to work harder");
+		}	else if (rating == 2) {
+			System.out.println("The current employee is doing well, but he needs progress");
+		}   else if (rating == 3) {
+			System.out.println("The current employee works satisfactorily");
+		}	else if (rating == 4) {
+			System.out.println("The current employee is doing very well");
+		}	else if (rating == 5) {
+			System.out.println("The current employee works excellent!");
+		}
 	}
 }	
 

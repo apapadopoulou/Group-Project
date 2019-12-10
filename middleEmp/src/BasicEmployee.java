e7b632211612554decba52be519b6862010c7f9c
eirinik80 <emnikolakopoulou@gmail.com>
2019-12-11 00:43:39 +0200

    Correcting my mistake

--- middleEmp/src/BasicEmployee.java
+++
@@ -1,5 +0,0 @@
-public class BasicEmployee extends Employee {
-	public BasicEmployee(String name, String surname, String telephone, String email, Department department, int id) {
-		super(name, surname, telephone, email, department);
-	}
-}

830742011383024d07a78d8cf5eefb7eea26aa98
alcabilone <alcabilone@gmail.com>
2019-12-12 22:01:58 +0200

    Changes to the Cosntructor and some comments.

--- BasicEmployee.java
+++ BasicEmployee.java
@@ -2,16 +2,27 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class BasicEmployee extends Employee {
-	private Department department;
-	public static ArrayList <BasicEmployee> basicEmployees = new ArrayList <BasicEmployee>(); 
+	
+	private Department department; //Department instance variable.
+	private String id; // ID instance variable.
+	public static ArrayList <BasicEmployee> basicEmployees = new ArrayList <BasicEmployee>(); // An ArrayList that contains all Basic Employees.
+	
+	/*Constructor for class BasicEmployee.*/
 	public BasicEmployee(String name, String surname,int telephone, String email, Date birthdate, String id, Department department) {
-		super(name, surname, telephone, email, birthdate);
+		super(name, surname, telephone, email, birthdate);//Calls the constructoer for the superclass Employee.
 		this.department = department;
-		/*the id is constructed using the department's id, the first two letters of the employees's name, the first two letters of the employee's surname
-	       * and the number of employees existing already in the department*/
-		id = String.valueOf(department.getId()) + name.substring(1,2) + surname.substring(1,2) + String.valueOf(department.getEmployeesOfDepartment().size());
-		basicEmployees.add(this);
-		employees.add(this);
+		/*
+		 * The id is constructed using the department's id,
+		 * the first two letters of the employees's name, 
+		 * the first two letters of the employee's surname
+	     * and the number of employees existing already in the department.
+	     */
+		id = String.valueOf(department.getId()) + name.substring(1,2)
+			+ surname.substring(1,2) 
+			+ String.valueOf(department.getEmployeesOfDepartment().size());
+		
+		basicEmployees.add(this); //Adds the object in the ArrayList of BasicEmployees.
+		employees.add(this); // I dont know what this does. Someone explain it here!!!!
 	}
 
 	public Department getDepartment() {
@@ -20,6 +31,10 @@ public class BasicEmployee extends Employee {
 
 	public void setDepartment(Department department) {
 		this.department = department;
+	}
+	
+	public String getID() {
+		return id;
 	}
 	// Method that changes the employee's department
 	public void changeDepartmentAndId(String empName, int depId) {

7ed43405bded670b1ff560059d2b67f04a0c3977
alcabilone <alcabilone@gmail.com>
2019-12-13 14:59:13 +0200

    Added evaluate and calculateDateDiff methods allong with comments

--- Evaluation.java
+++ Evaluation.java
@@ -1,19 +1,36 @@
-import java.util.Calendar;
+import java.util.Date; //Used to get the Time.
 import java.util.ArrayList;
 import java.sql.*;
 
 public class Evaluation {
-	public static double evaluate(BasicEmployee emp, Task task) { // WIP!!!!!!
-		
-		return 0.0;
+	
+	/*
+	 * Basic method which is called to evaluate a Task.
+	 * Checks if the task is for single-employee or a group task and calls the corresponding
+	 * moethod for evaluating the task.
+	 */
+	public static double evaluate(Task task) {
+		double score = 0.0;
+		if (!task.isGroupTask()) { //Checks if a task is group task or not.
+			score = evalSingleTask(task); //Calls method for evaluating single-employee Tasks.
+		} else {
+			score = evalGroupTask(task); //Calls method for evaluating group Tasks.
+		}
+		return score;
 	}
 	
-	public static double evaluate(ArrayList<Employee> group, GroupTask task) { // WIP!!!!!!!!
+	public static double evalSingleTask(Task task) {
 		
-		return 0.0;
 	}
 	
-	public static ArrayList<Double> getEvalHistory(String empid) {
+	public static double evalGroupTask(Task task) {
+		
+	}
+	/*
+	 *Method responsible for retrieving past evaluations of employee's performance on his tasks.
+	 *Returns an ArrayLIst of type Double which contains all of an employee's previous evaluations. 
+	 */
+	public static ArrayList<Double> getEvalHistory(String id) {
 		ArrayList<Double> evalHistory = new ArrayList<Double>();
 		/*URL of database with username and password.*/
 		String url ="jdbc:sqlserver://sqlserver.dmst.aueb.gr:1433;" + 
@@ -38,7 +55,7 @@ public class Evaluation {
 			/*Creates the statement*/
 			stmt = dbcon.createStatement();
 			/*Executes the given query that returns the History of Evaluations for the particular BasicEmployee using his ID.*/
-			rs = stmt.executeQuery("SELECT evaluation FROM JEvaluations WHERE empID=" + empid );
+			rs = stmt.executeQuery("SELECT evaluation FROM JEvaluations WHERE empID=" + id );
 			/*Does a loop for every row it finds.*/
 			while (rs.next()) {
 				double evaluation = rs.getDouble("evaluation");//Returns the evalutaion value from this row.
@@ -53,5 +70,20 @@ public class Evaluation {
 			System.out.println("SQLException: " + e.getMessage());
 		}
 		return evalHistory; // Returns the ArrayList.
+	}
+	
+	/*
+	 * Method to calculate the difference between completion date and due date in percentage.
+	 * Returns the percentage difference between total time to complete the task and remaining time to Due Date.
+	 */
+	public static double calculateDateDiff(Date startDate, Date dueDate, Date completionDate) {
+		long startDiff = dueDate.getTime() - startDate.getTime(); //Calculates the difference between DueDate and startDate in milliseconds.
+		long startDiffMins = startDiff / (60 * 1000) % 60; //Calculates the difference between dueDate and startDate in minutes.
+		
+		long completionDiff = completionDate.getTime() - dueDate.getTime();
+		long completionDiffMins =completionDiff / (60 * 1000) % 60;
+		
+		double percentageDiff = completionDiffMins / startDiffMins; // Calculates the percentage difference between total time to complete the task and completion time.
+		return percentageDiff; //Returns the percentage difference.
 	}
 }

0882d72bf8b9f9224e4e2866e58782f8214bc768
Natalia Tsamtsuri <t8180138@aueb.gr>
2019-12-13 17:05:48 +0200

    added a comment

--- DateformatException.java
+++ DateformatException.java
@@ -1,3 +1,4 @@
+/* Verifies that the user is going to insert date of birth according to "dd/MM/yyyy" */
 public class DateformatException extends Exception {
     public DateformatException() { }
     public DateformatException(String msg) {

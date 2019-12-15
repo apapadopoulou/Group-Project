de9a8f076b73030a4d59325b108069087512e3b6
alcabilone <alcabilone@gmail.com>
2019-12-15 22:20:04 +0200

    Changes to evaluation algorithm and variable names

--- Evaluation.java
+++ Evaluation.java
@@ -7,10 +7,10 @@ public class Evaluation {
 	/*URL of database with username and password.*/
 	public static String url ="jdbc:sqlserver://sqlserver.dmst.aueb.gr:1433;" + 
 			"databaseName=DB56;user=G556;password=939wd5890;";
-	public static final double IMPORTANCERATE = 0.8;
-	public static final double STARTINGEVAL = 10.0;
-	static final double EVALBONUS = 0.4;
-	static final double PERFORMANCEDIFFRATE = 0.1; //Final variable used to calculate if the task score has had an increase at least as big as this one compared to the average scores.
+	public static final double IMPORTANCE_RATE = 0.8;
+	public static final double STARTING_EVAL = 10.0;
+	static final double EVAL_BONUS = 0.4;
+	static final double PERFORMANCE_DIFF_RATE = 0.1; //Final variable used to calculate if the task score has had an increase at least as big as this one compared to the average scores.
 	/*
 	 * Basic method which is called to evaluate a Task.
 	 * Checks if the task is for single-employee or a group task and calls the corresponding
@@ -21,6 +21,7 @@ public class Evaluation {
 			double score = 0.0;
 			score = evalSingleTask(task); //Calls method for evaluating single-employee Tasks.
 			saveToDb(task, score);
+			task.setTaskScore(score);
 		} else {
 			double[] scores = new double[task.getEmpIDs().size()];
 			scores = evalGroupTask(task); //Calls method for evaluating group Tasks.
@@ -41,150 +42,11 @@ public class Evaluation {
 		if (evalHistory.size() != 0) {
 			evalAvg = evalSum / evalHistory.size(); //Calculates the average score of all the evaluations.
 		}	else {
-			evalAvg = STARTINGEVAL;
+			evalAvg = STARTING_EVAL;
 		}
 		double percentageDiff = calculateDateDiff(task.getStartDate(), task.getDueDate(),task.getCompletionDate() );
 		double timeScore;
-		double taskLevel = task.getDifficulty() * (1 - IMPORTANCERATE) + task.getImportance() * IMPORTANCERATE; //Calculates the Task level based on the difficulty and importance. 
-		if (taskLevel > 7.0) {
-			double timeDiffRate = taskLevel / 10;
-			if (percentageDiff >= (timeDiffRate - 0.1 * taskLevel)) {
-				timeScore = 10.0;
-			} else if (percentageDiff >= (timeDiffRate - 0.2 * taskLevel)) {
-				timeScore = 9.5;
-			} else if (percentageDiff >= (timeDiffRate - 0.3 * taskLevel)) {
-				timeScore = 8.2;
-			} else if (percentageDiff >= (timeDiffRate - 0.4 * taskLevel)) {
-				timeScore = 6.6;
-			} else if (percentageDiff >= (timeDiffRate - 0.5 * taskLevel)) {
-				timeScore = 5.3;
-			} else if (percentageDiff >= (timeDiffRate - 0.6 * taskLevel)) {
-				timeScore = 3.8;
-			} else if (percentageDiff >= (timeDiffRate - 0.7 * taskLevel)) {
-				timeScore = 2.4;
-			} else if (percentageDiff >= (timeDiffRate - 0.8 * taskLevel)) {
-				timeScore = 0.9;
-			} else if (percentageDiff >= (timeDiffRate - 0.9 * taskLevel)) {
-				timeScore = 0.6;
-			} else if (percentageDiff >= (timeDiffRate - 0.95 * taskLevel)) {
-				timeScore = 0.3;
-			} else {
-				timeScore = 0.0;
-			}
-		} else if (taskLevel > 5.5){
-			double timeDiffRate = (taskLevel / 10) + 0.1;
-			if (percentageDiff >= (timeDiffRate - 0.1 * taskLevel)) {
-				timeScore = 10.0;
-			} else if (percentageDiff >= (timeDiffRate - 0.2 * taskLevel)) {
-				timeScore = 7.5;
-			} else if (percentageDiff >= (timeDiffRate - 0.3 * taskLevel)) {
-				timeScore = 6.0;
-			} else if (percentageDiff >= (timeDiffRate - 0.4 * taskLevel)) {
-				timeScore = 5.1;
-			} else if (percentageDiff >= (timeDiffRate - 0.5 * taskLevel)) {
-				timeScore = 3.1;
-			} else if (percentageDiff >= (timeDiffRate - 0.6 * taskLevel)) {
-				timeScore = 1.1;
-			} else if (percentageDiff >= (timeDiffRate - 0.7 * taskLevel)) {
-				timeScore = 0.4;
-			} else if (percentageDiff >= (timeDiffRate - 0.8 * taskLevel)) {
-				timeScore = 0.2;
-			} else if (percentageDiff >= (timeDiffRate - 0.9 * taskLevel)) {
-				timeScore = 0.1;
-			} else {
-				timeScore = 0.0;
-			}
-		} else if (taskLevel > 4.5){
-			double timeDiffRate = (taskLevel / 10) + 0.3;
-			if (percentageDiff >= (timeDiffRate - 0.1 * taskLevel)) {
-				timeScore = 10.0;
-			} else if (percentageDiff >= (timeDiffRate - 0.2 * taskLevel)) {
-				timeScore = 7.5;
-			} else if (percentageDiff >= (timeDiffRate - 0.3 * taskLevel)) {
-				timeScore = 6.0;
-			} else if (percentageDiff >= (timeDiffRate - 0.4 * taskLevel)) {
-				timeScore = 5.1;
-			} else if (percentageDiff >= (timeDiffRate - 0.5 * taskLevel)) {
-				timeScore = 3.1;
-			} else if (percentageDiff >= (timeDiffRate - 0.6 * taskLevel)) {
-				timeScore = 1.1;
-			} else if (percentageDiff >= (timeDiffRate - 0.7 * taskLevel)) {
-				timeScore = 0.4;
-			} else if (percentageDiff >= (timeDiffRate - 0.8 * taskLevel)) {
-				timeScore = 0.2;
-			} else if (percentageDiff >= (timeDiffRate - 0.9 * taskLevel)) {
-				timeScore = 0.1;
-			} else {
-				timeScore = 0.0;
-			}
-		} else {
-			double timeDiffRate = (taskLevel / 10) + 0.45;
-			if (percentageDiff >= timeDiffRate) {
-				timeScore = 10.0;
-			}else if (percentageDiff >= (timeDiffRate - 0.05 * taskLevel)) {
-				timeScore = 6.7;
-			} else if (percentageDiff >= (timeDiffRate - 0.1 * taskLevel)) {
-				timeScore = 5.4;
-			} else if (percentageDiff >= (timeDiffRate - 0.2 * taskLevel)) {
-				timeScore = 4.6;
-			} else if (percentageDiff >= (timeDiffRate - 0.3 * taskLevel)) {
-				timeScore = 3.4;
-			} else if (percentageDiff >= (timeDiffRate - 0.4 * taskLevel)) {
-				timeScore = 2.0;
-			} else if (percentageDiff >= (timeDiffRate - 0.5 * taskLevel)) {
-				timeScore = 0.8;
-			} else if (percentageDiff >= (timeDiffRate - 0.6 * taskLevel)) {
-				timeScore = 0.3;
-			} else if (percentageDiff >= (timeDiffRate - 0.7 * taskLevel)) {
-				timeScore = 0.2;
-			} else if (percentageDiff >= (timeDiffRate - 0.8 * taskLevel)) {
-				timeScore = 0.1;
-			} else {
-				timeScore = 0.0;
-			}
-		}
-		double scoreNow;
-		if (taskLevel > 7.0) {
-			scoreNow = 10 - ((10 - timeScore ) * (taskLevel / 10) + 0.05);
-		} else if (taskLevel > 5.5) {
-			scoreNow = 10 - ((10 - timeScore ) * ((taskLevel / 10) + 0.1));
-		} else if (taskLevel > 4.5) {
-			scoreNow = 10 - ((10 - timeScore ) * ((taskLevel / 10) + 0.15));
-		} else {
-			scoreNow = 10 - ((10 - timeScore ) * ((taskLevel / 10) + 0.2));
-		}
-		double finalScore; //Variable for the final score.
-		if (scoreNow > (evalAvg + (scoreNow * PERFORMANCEDIFFRATE))) { //Checks if the score is increased by at least the given percentage when compared to the average score.
-			if (scoreNow <= (10.0 - EVALBONUS)) {
-				finalScore = scoreNow + EVALBONUS;
-			} else if (scoreNow <= (10.0 - (EVALBONUS / 2))) {
-				finalScore = scoreNow + (EVALBONUS / 2);
-			} else {
-				finalScore = scoreNow;
-			}
-		} else {
-			finalScore = scoreNow;
-		}
-		return finalScore; //REturns the final score.
-	}
-	
-	public static double[] evalGroupTask(Task task) {
-		double [] evalAvg = new double [task.getEmpIDs().size()]; 
-		for (int j = 0; j < task.getEmpIDs().size(); j++) {
-			ArrayList<Double> evalHistory = getEvalHistory(task.getEmpIDs().get(j));
-			double evalSum= 0.0;
-			for (int i = 0; i < evalHistory.size(); i++) {
-				evalSum += evalHistory.get(i); //Adds the score for every previous evaluation.
-			}
-			if (evalHistory.size() != 0) {
-				evalAvg[j] = evalSum / evalHistory.size(); //Calculates the average score of all the evaluations.
-			}	else {
-				evalAvg[j] = STARTINGEVAL;
-			}
-		}
-		double percentageDiff = calculateDateDiff(task.getStartDate(), task.getDueDate(),task.getCompletionDate() );
-		double timeScore = 10.0;
-		double taskLevel = task.getDifficulty() * (1 - IMPORTANCERATE) + task.getImportance() * IMPORTANCERATE; //Calculates the Task level based on the difficulty and importance. 
+		double taskLevel = task.getDifficulty() * (1 - IMPORTANCE_RATE) + task.getImportance() * IMPORTANCE_RATE; //Calculates the Task level based on the difficulty and importance. 
 		if (taskLevel > 7.0) {
 			double timeDiffRate = taskLevel / 10;
 			if (percentageDiff >= (timeDiffRate - 0.1 * timeDiffRate)) {
@@ -219,21 +81,23 @@ public class Evaluation {
 			} else if (percentageDiff >= (timeDiffRate - 0.2 * timeDiffRate)) {
 				timeScore = 7.5;
 			} else if (percentageDiff >= (timeDiffRate - 0.3 * timeDiffRate)) {
-				timeScore = 6.0;
+				timeScore = 6.5;
 			} else if (percentageDiff >= (timeDiffRate - 0.4 * timeDiffRate)) {
-				timeScore = 5.1;
+				timeScore = 5.8;
 			} else if (percentageDiff >= (timeDiffRate - 0.5 * timeDiffRate)) {
-				timeScore = 3.1;
+				timeScore = 4.4;
 			} else if (percentageDiff >= (timeDiffRate - 0.6 * timeDiffRate)) {
-				timeScore = 1.1;
+				timeScore = 3.1;
 			} else if (percentageDiff >= (timeDiffRate - 0.7 * timeDiffRate)) {
-				timeScore = 0.4;
+				timeScore = 2.6;
 			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
-				timeScore = 0.2;
+				timeScore = 1.9;
 			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
-				timeScore = 0.1;
+				timeScore = 1.8;
 			} else if (percentageDiff < 0.0){
 				timeScore = 0.0;
+			} else {
+				timeScore = 1.5;
 			}
 		} else if (taskLevel > 4.5){
 			double timeDiffRate = (taskLevel / 10) + 0.3;
@@ -254,18 +118,20 @@ public class Evaluation {
 			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
 				timeScore = 0.2;
 			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
-				timeScore = 0.1;
+				timeScore = 0.15;
 			} else if (percentageDiff < 0.0){
 				timeScore = 0.0;
+			} else {
+				timeScore = 0.1;
 			}
 		} else {
 			double timeDiffRate = (taskLevel / 10) + 0.45;
 			if (percentageDiff >= timeDiffRate) {
 				timeScore = 10.0;
 			}else if (percentageDiff >= (timeDiffRate - 0.05 * timeDiffRate)) {
-				timeScore = 6.7;
+				timeScore = 8.7;
 			} else if (percentageDiff >= (timeDiffRate - 0.1 * timeDiffRate)) {
-				timeScore = 5.4;
+				timeScore = 6.4;
 			} else if (percentageDiff >= (timeDiffRate - 0.2 * timeDiffRate)) {
 				timeScore = 4.6;
 			} else if (percentageDiff >= (timeDiffRate - 0.3 * timeDiffRate)) {
@@ -275,15 +141,166 @@ public class Evaluation {
 			} else if (percentageDiff >= (timeDiffRate - 0.5 * timeDiffRate)) {
 				timeScore = 0.8;
 			} else if (percentageDiff >= (timeDiffRate - 0.6 * timeDiffRate)) {
-				timeScore = 0.3;
+				timeScore = 0.5;
 			} else if (percentageDiff >= (timeDiffRate - 0.7 * timeDiffRate)) {
-				timeScore = 0.2;
+				timeScore = 0.4;
 			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
-				timeScore = 0.1;
+				timeScore = 0.3;
 			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
-				timeScore = 0.01;
+				timeScore = 0.2;
 			} else if (percentageDiff < 0.0){
 				timeScore = 0.0;
+			} else {
+				timeScore = 0.1;
+			}
+		}
+		double scoreNow;
+		if (taskLevel > 7.0) {
+			scoreNow = 10 - ((10 - timeScore ) * (taskLevel / 10) + 0.05);
+		} else if (taskLevel > 5.5) {
+			scoreNow = 10 - ((10 - timeScore ) * ((taskLevel / 10) + 0.1));
+		} else if (taskLevel > 4.5) {
+			scoreNow = 10 - ((10 - timeScore ) * ((taskLevel / 10) + 0.15));
+		} else {
+			scoreNow = 10 - ((10 - timeScore ) * ((taskLevel / 10) + 0.2));
+		}
+		double finalScore; //Variable for the final score.
+		if (scoreNow > (evalAvg + (scoreNow * PERFORMANCE_DIFF_RATE))) { //Checks if the score is increased by at least the given percentage when compared to the average score.
+			if (scoreNow <= (10.0 - EVAL_BONUS)) {
+				finalScore = scoreNow + EVAL_BONUS;
+			} else if (scoreNow <= (10.0 - (EVAL_BONUS / 2))) {
+				finalScore = scoreNow + (EVAL_BONUS / 2);
+			} else {
+				finalScore = scoreNow;
+			}
+		} else {
+			finalScore = scoreNow;
+		}
+		return finalScore; //REturns the final score.
+	}
+	
+	public static double[] evalGroupTask(Task task) {
+		double [] evalAvg = new double [task.getEmpIDs().size()]; 
+		for (int j = 0; j < task.getEmpIDs().size(); j++) {
+			ArrayList<Double> evalHistory = getEvalHistory(task.getEmpIDs().get(j));
+			double evalSum= 0.0;
+			for (int i = 0; i < evalHistory.size(); i++) {
+				evalSum += evalHistory.get(i); //Adds the score for every previous evaluation.
+			}
+			if (evalHistory.size() != 0) {
+				evalAvg[j] = evalSum / evalHistory.size(); //Calculates the average score of all the evaluations.
+			}	else {
+				evalAvg[j] = STARTING_EVAL;
+			}
+		}
+		double percentageDiff = calculateDateDiff(task.getStartDate(), task.getDueDate(),task.getCompletionDate() );
+		double timeScore = 10.0;
+		double taskLevel = task.getDifficulty() * (1 - IMPORTANCE_RATE) + task.getImportance() * IMPORTANCE_RATE; //Calculates the Task level based on the difficulty and importance. 
+		if (taskLevel > 7.0) {
+			double timeDiffRate = taskLevel / 10;
+			if (percentageDiff >= (timeDiffRate - 0.1 * timeDiffRate)) {
+				timeScore = 10.0;
+			} else if (percentageDiff >= (timeDiffRate - 0.2 * timeDiffRate)) {
+				timeScore = 9.5;
+			} else if (percentageDiff >= (timeDiffRate - 0.3 * timeDiffRate)) {
+				timeScore = 8.2;
+			} else if (percentageDiff >= (timeDiffRate - 0.4 * timeDiffRate)) {
+				timeScore = 7.8;
+			} else if (percentageDiff >= (timeDiffRate - 0.5 * timeDiffRate)) {
+				timeScore = 7.3;
+			} else if (percentageDiff >= (timeDiffRate - 0.6 * timeDiffRate)) {
+				timeScore = 6.8;
+			} else if (percentageDiff >= (timeDiffRate - 0.7 * timeDiffRate)) {
+				timeScore = 6.5;
+			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
+				timeScore = 6.2;
+			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
+				timeScore = 5.6;
+			} else if (percentageDiff >= (timeDiffRate - 0.95 * timeDiffRate)) {
+				timeScore = 4.3;
+			} else if (percentageDiff < 0.0) {
+				timeScore = 0.0;
+			} else {
+				timeScore = 3.0;
+			}
+		} else if (taskLevel > 5.5){
+			double timeDiffRate = (taskLevel / 10) + 0.1;
+			if (percentageDiff >= (timeDiffRate - 0.1 * timeDiffRate)) {
+				timeScore = 10.0;
+			} else if (percentageDiff >= (timeDiffRate - 0.2 * timeDiffRate)) {
+				timeScore = 7.5;
+			} else if (percentageDiff >= (timeDiffRate - 0.3 * timeDiffRate)) {
+				timeScore = 6.5;
+			} else if (percentageDiff >= (timeDiffRate - 0.4 * timeDiffRate)) {
+				timeScore = 5.8;
+			} else if (percentageDiff >= (timeDiffRate - 0.5 * timeDiffRate)) {
+				timeScore = 4.4;
+			} else if (percentageDiff >= (timeDiffRate - 0.6 * timeDiffRate)) {
+				timeScore = 3.1;
+			} else if (percentageDiff >= (timeDiffRate - 0.7 * timeDiffRate)) {
+				timeScore = 2.6;
+			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
+				timeScore = 1.9;
+			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
+				timeScore = 1.8;
+			} else if (percentageDiff < 0.0){
+				timeScore = 0.0;
+			} else {
+				timeScore = 1.5;
+			}
+		} else if (taskLevel > 4.5){
+			double timeDiffRate = (taskLevel / 10) + 0.3;
+			if (percentageDiff >= (timeDiffRate - 0.1 * timeDiffRate)) {
+				timeScore = 10.0;
+			} else if (percentageDiff >= (timeDiffRate - 0.2 * timeDiffRate)) {
+				timeScore = 7.5;
+			} else if (percentageDiff >= (timeDiffRate - 0.3 * timeDiffRate)) {
+				timeScore = 6.0;
+			} else if (percentageDiff >= (timeDiffRate - 0.4 * timeDiffRate)) {
+				timeScore = 5.1;
+			} else if (percentageDiff >= (timeDiffRate - 0.5 * timeDiffRate)) {
+				timeScore = 3.1;
+			} else if (percentageDiff >= (timeDiffRate - 0.6 * timeDiffRate)) {
+				timeScore = 1.1;
+			} else if (percentageDiff >= (timeDiffRate - 0.7 * timeDiffRate)) {
+				timeScore = 0.4;
+			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
+				timeScore = 0.2;
+			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
+				timeScore = 0.15;
+			} else if (percentageDiff < 0.0){
+				timeScore = 0.0;
+			} else {
+				timeScore = 0.1;
+			}
+		} else {
+			double timeDiffRate = (taskLevel / 10) + 0.45;
+			if (percentageDiff >= timeDiffRate) {
+				timeScore = 10.0;
+			}else if (percentageDiff >= (timeDiffRate - 0.05 * timeDiffRate)) {
+				timeScore = 8.7;
+			} else if (percentageDiff >= (timeDiffRate - 0.1 * timeDiffRate)) {
+				timeScore = 6.4;
+			} else if (percentageDiff >= (timeDiffRate - 0.2 * timeDiffRate)) {
+				timeScore = 4.6;
+			} else if (percentageDiff >= (timeDiffRate - 0.3 * timeDiffRate)) {
+				timeScore = 3.4;
+			} else if (percentageDiff >= (timeDiffRate - 0.4 * timeDiffRate)) {
+				timeScore = 2.0;
+			} else if (percentageDiff >= (timeDiffRate - 0.5 * timeDiffRate)) {
+				timeScore = 0.8;
+			} else if (percentageDiff >= (timeDiffRate - 0.6 * timeDiffRate)) {
+				timeScore = 0.5;
+			} else if (percentageDiff >= (timeDiffRate - 0.7 * timeDiffRate)) {
+				timeScore = 0.4;
+			} else if (percentageDiff >= (timeDiffRate - 0.8 * timeDiffRate)) {
+				timeScore = 0.3;
+			} else if (percentageDiff >= (timeDiffRate - 0.9 * timeDiffRate)) {
+				timeScore = 0.2;
+			} else if (percentageDiff < 0.0){
+				timeScore = 0.0;
+			} else {
+				timeScore = 0.1;
 			}
 		}
 		double scoreNow = 10.0;
@@ -298,11 +315,11 @@ public class Evaluation {
 		}
 		double finalScore[]= new double [task.getEmpIDs().size()]; //Variable for the final score.
 		for (int j = 0; j < task.getEmpIDs().size(); j++) {
-		if (scoreNow > (evalAvg[j] + (scoreNow * PERFORMANCEDIFFRATE))) { //Checks if the score is increased by at least the given percentage when compared to the average score.
-			if (scoreNow <= (10.0 - EVALBONUS)) {
-				finalScore[j] = scoreNow + EVALBONUS;
-			} else if (scoreNow <= (10.0 - (EVALBONUS / 2))) {
-				finalScore[j] = scoreNow + (EVALBONUS / 2);
+		if (scoreNow > (evalAvg[j] + (scoreNow * PERFORMANCE_DIFF_RATE))) { //Checks if the score is increased by at least the given percentage when compared to the average score.
+			if (scoreNow <= (10.0 - EVAL_BONUS)) {
+				finalScore[j] = scoreNow + EVAL_BONUS;
+			} else if (scoreNow <= (10.0 - (EVAL_BONUS / 2))) {
+				finalScore[j] = scoreNow + (EVAL_BONUS / 2);
 			} else {
 				finalScore[j] = scoreNow;
 			}
@@ -378,10 +395,10 @@ public class Evaluation {
 			stmt = dbcon.createStatement();
 			if (!task.isGroupTask()) { //Checks if its a group task or not.
 				/*Executes the given statement that saves the evaluation score and the employee's ID. */
-				stmt.executeUpdate("INSERT INTO JEvaluations (empID, evaluation) VALUES (" + task.getEmpID() + ", " + score + ");");
+				stmt.executeUpdate("INSERT INTO JEvaluations (taskID, empID, evaluation) VALUES (" + task.getTaskID() + ", " + task.getEmpID() + ", " + score + ");");
 			} else {
 				for (int i =0; i < task.getEmpIDs().size(); i++) {
-					stmt.executeUpdate("INSERT INTO JEvaluations (empID, evaluation) VALUES (" + task.getEmpIDs().get(i) + ", " + score + ");");
+					stmt.executeUpdate("INSERT INTO JEvaluations (taskID, empID, evaluation) VALUES (" + task.getTaskID() + ", " + task.getEmpIDs().get(i) + ", " + score + ");");
 				}
 			}
 				

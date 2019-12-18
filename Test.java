6b02f2e3bf163c93dd43983549981ebfea39cd85
alcabilone <alcabilone@gmail.com>
2019-12-18 20:38:59 +0200

    updated test class

--- Test.java
+++ Test.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Date;
+
 public class Test {
-  public static void main(String args []) {
-    ArrayList <Employee> list = new ArrayList <Employee>();
-    Department dep1 = new Department("de[ne",list);
-    Employee emp1 = new HR_consultant("yeyey","perkas","bigoz",dep1);
-    System.out.println(emp1.getId());
-    System.out.println(emp1.getJobTitle());
-    HR_consultant emp2 = new HR_consultant("vasilis","xifaras","342bigozdqqwqdq",dep1);
-    System.out.println(emp2.getId());
-    System.out.println(emp2.getJobTitle());
-    System.out.println(emp1.toString());
-    System.out.println(emp2.toString());
-           }
- }   
-            
-             
+	public static void main(String args []) {
+		DBcon.loadBasicEmployees();
+		//System.out.println("Loaded BASIC EMPLOYEES");
+		Date birthDate = new Date();
+		Department dep = new Department("Sales");
+		System.out.println("Created DEPARTMENT");
+		Employee emp = new BasicEmployee("vasilis", "Xifaras", "6942796775", "alcabilone@gmail.com", birthDate, 1);
+		System.out.println("Created BASIC EMPLOYEE");
+	}
+ } 

3d7374a064ad7cb055adcaf91c789cf43c1e18bc
Angela Papadopoulou <angelikeepap@gmail.com>
2019-12-19 13:58:03 +0200

    created main method and javadoc documentation

--- BusyB.java
+++ BusyB.java
@@ -1,6 +1,11 @@
 public class BusyB {
-	public boolean init = false; 
-	public void setInit() {
-		init = true;
+	/**This is the main class of the app. User runs the app for the first time from the command line, using an argument.
+	 * Main method checks if the first cell of args array is null. If it is not, the initial data entry begins to run.
+	 * @param args
+	 */
+	public static void main (String[] args) {
+		if (args[0] != null) {
+			
+		}
 	}
 }

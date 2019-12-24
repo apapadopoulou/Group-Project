75741b8e087905d567bd5caee7d0b842673daa80
eirinik80 <emnikolakopoulou@gmail.com>
2019-12-24 18:11:09 +0200

    400th commitgit add CreateEvent.java FirstWindow.java!

--- CreateEvent.java
+++ CreateEvent.java
@@ -17,13 +17,13 @@ import javax.swing.Timer;
  *
  * @author User
  */
-public class CreatEvent extends javax.swing.JFrame {
+public class CreateEvent extends javax.swing.JFrame {
 
     /**
      * Creates new form CreatEvent
      */
     private int n;
-    public CreatEvent(int n) {
+    public CreateEvent(int n) {
         this.n = n; 
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         initComponents();
@@ -91,7 +91,7 @@ public class CreatEvent extends javax.swing.JFrame {
         jComboBox7 = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setIconImage(Toolkit.getDefaultToolkit().getImage(CreatEvent.class.getResource("/images/smallLogo.PNG")));
+        setIconImage(Toolkit.getDefaultToolkit().getImage(CreateEvent.class.getResource("/images/smallLogo.PNG")));
 
         jPanel1.setBackground(new java.awt.Color(255, 222, 89));
 
@@ -353,20 +353,20 @@ public class CreatEvent extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CreatEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CreatEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CreatEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CreatEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateEvent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CreatEvent(1).setVisible(true);
+                new CreateEvent(1).setVisible(true);
             }
         });
     }

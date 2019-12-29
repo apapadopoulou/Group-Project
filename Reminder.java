2055e16766e68c749f9a3a019f9c317fbf1f293d
Katerina Divini <k_divini@yahoo.gr>
2019-12-29 03:46:46 +0200

    created the Reminder class which extends Program

---
+++ Reminder.java
@@ -0,0 +1,17 @@
+
+public class Reminder extends Program {
+	private String time;
+	private String description;
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}

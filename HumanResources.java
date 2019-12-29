4b3f4129adc63febbf536ed4ac2151c4dbbc5486
panos papasotiriou <panos1606@gmail.com>
2019-12-29 22:04:50 +0200

    checkstyled

--- HumanResources.java
+++ HumanResources.java
@@ -5,28 +5,33 @@ import java.util.Date;
 /*class HumanResources*/
 public class HumanResources extends Employee {
     /*parameters for class HumanResources*/
-	HumanResources hr = new HumanResources();
+    HumanResources hr = new HumanResources();
 
     /*Constructor for class HumanResources*/
-    public HumanResources(String name, String surname, String phonenumber, String email, String id, date birthdate) {
+    public HumanResources(String name, String surname, String phonenumber,
+    		String email, String id, date birthdate) {
 		super(name, surname, phonenumber, email, id, birthdate);
 		employees.add(this);
     }
 
-/*Using for loop in order to find out if the employee is a HR Director and to display the salary of the current employee*/
+/*Using for loop in order to find out if the employee is a HR Director
+ *  and to display the salary of the current employee*/
      /*  for (int counter = 0; counter < employees.size(); counter++) {
         hr.login();/*Calling method login() from Account class*/
        /* if (Department.getDepartments().get(counter).getName.equals("HR")) {
-            System.out.println("The current employee is registered at HR department!");
+            System.out.println("The current employee"
+            + "is registered at HR department!");
             Menu1();/*Calling method Menu1() */
      /*   } else {
-            System.out.println("The current employee is not registered at HR department.");
+            System.out.println("The current employee is not registered"
+            + " at HR department.");
         }
        }
         /*Creating method to display the menu to the employee*/
         public static void Display_Menu() {
-     	   System.out.println("1) Edit Salaries\n 2) Edit Timeoffs \n 3) See Calendar");
-     	   System.out.print( "Selection:");
+     	   System.out.println("1) Edit Salaries\n 2) Edit Timeoffs \n 3)"
+     	   		+ " See Calendar");
+     	   System.out.print("Selection:");
         }
         /*Creating method Menu1 to make menu operating*/
         public static void Menu1() {

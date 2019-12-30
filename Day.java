7f772eb59a265274463d6d5a9f136f2e9d9d202d
Sarantos Moraitis <praetorians12345@gmail.com>
2019-12-30 15:20:49 +0200

    Added javadoc comments

--- Day.java
+++ Day.java
@@ -4,32 +4,46 @@ import java.util.ArrayList;
 import java.util.Date;
 import javax.swing.*;
 
+/**
+ *@author BusyB 
+ */
+
 public class Day {
   private ArrayList<Program> dailyProgram = new ArrayList<Program>();
   private Date date;
   public Day(Date d) {
     date = d;
   }
-
+/**
+ *Gets the daily program.
+ *@return daily program
+ */
   public ArrayList<Program> getDailyProgram() {
    	return dailyProgram;
   }
-
+ /**
+  *Sets the daily program.
+  */
   public void setDailyProgram(ArrayList<Program> dailyProgram) {
     this.dailyProgram = dailyProgram;
   }
 
-
+/**
+ *Gets the date.
+ *@return date 
+ */
   public Date getDate() {
     return date;
  }
 
-
+/**
+ *Sets the date.
+ */
   public void setDate(Date date) {
 	this.date = date;
   }
 
-  /* public void displayDay(LocalDate date) {
+  /** public void displayDay(LocalDate date) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyy");
     System.out.println(date.format(formatter));
   }
@@ -39,3 +53,6 @@ public class Day {
     dailyProgram.add(p);
   }
 }
+/** 
+*End of Day class.
+*/

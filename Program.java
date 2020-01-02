5c1a2ce6039ee187277b807a0849b441a18d0fac
panos papasotiriou <panos1606@gmail.com>
2020-01-02 17:11:11 +0200

    checkstyled

--- Program.java
+++ Program.java
@@ -4,15 +4,18 @@ import java.util.Date;
 
 /*This class creates Event and Task objects.*/
 public abstract class Program {
-	Calendar c;
-	protected Date date;
-	public static ArrayList<Program> allPrograms = new ArrayList<Program>();
-	public Program() {}
-	/*current_date = calendar.get(Calendar.DATE)+"/"+calendar.get(Calendar.MONTH)+"/"+calendar.get(Calendar.YEAR); na to tsekaroume an xreiazetai kapou*/
-	public void setDate(Date date) {
-		this.date=date;
-	}
-	public Date getDate() {
-		return date;
-	}
+    private Calendar c;
+    protected Date date;
+    public static ArrayList<Program> allPrograms = new ArrayList<Program>();
+    public Program() {
+     /*current_date = calendar.get(Calendar.DATE)+"/"
+      * +calendar.get(Calendar.MONTH)
+     * +"/"+calendar.get(Calendar.YEAR); na to tsekaroume an xreiazetai kapou*/
+    }
+    public void setDate(Date date) {
+      this.date = date;
+    }
+    public Date getDate() {
+      return date;
+    }
 }

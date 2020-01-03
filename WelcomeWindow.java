95b8ffb53dbf8b9dc493b497b1a6f9a004bcab6f
eirinik80 <emnikolakopoulou@gmail.com>
2020-01-03 14:58:02 +0200

    Connection

--- WelcomeWindow.java
+++ WelcomeWindow.java
@@ -12,9 +12,7 @@ public class WelcomeWindow extends javax.swing.JFrame {
     /**
      * Creates new form WelcomeWindow
      */
-    private int n;
-    private Employee emp;
-
+    
     public WelcomeWindow() {
         initComponents();
         setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -98,7 +96,7 @@ public class WelcomeWindow extends javax.swing.JFrame {
         // TODO add your handling code here:
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FirstWindow(emp).setVisible(true);
+                new LoginWindow().setVisible(true);
             }
         });
         this.dispose();
@@ -113,7 +111,7 @@ public class WelcomeWindow extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
+        /*try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());

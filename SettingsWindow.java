c4e0c5733aee6aba5d8fb835c269ee82c29f42aa
eirinik80 <emnikolakopoulou@gmail.com>
2020-01-10 21:58:08 +0200

    Connection is done

--- SettingsWindow.java
+++ SettingsWindow.java
@@ -47,7 +47,6 @@ public class SettingsWindow extends javax.swing.JFrame {
                 Date d = new Date();
                 SimpleDateFormat s = new SimpleDateFormat("hh:mm:ss a");
                 currentTime.setText(s.format(d));
-
             }
         }
         ).start();
@@ -274,48 +273,14 @@ public class SettingsWindow extends javax.swing.JFrame {
 
     private void change_pActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_change_pActionPerformed
         // TODO add your handling code here:
-        new ChangeEmailWindow().setVisible(true);
+        new ChangeEmailWindow(emp).setVisible(true);
     }//GEN-LAST:event_change_pActionPerformed
 
     private void change_eActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_change_eActionPerformed
         // TODO add your handling code here:
-        new ChangePasswordWindow().setVisible(true);
+        new ChangePasswordWindow(emp).setVisible(true);
     }//GEN-LAST:event_change_eActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SettingsWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SettingsWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SettingsWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SettingsWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-       /* java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SettingsWindow(1).setVisible(true);
-            }
-        }); */
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel birthdate;

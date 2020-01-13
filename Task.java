8be3bf52bc979330ec6cd38be0a179f5c8b17a7f
eirinik80 <emnikolakopoulou@gmail.com>
2020-01-13 15:21:03 +0200

    Notes

--- Task.java
+++ Task.java
@@ -159,17 +159,7 @@ public class Task extends Program implements Comparable<Task>{
 	 * Returns the done field
 	 * @return done
 	 */
-    public boolean getDone(){
-    	return done;
-    }
-    /**
-     * Sets the done field
-     * @param done
-     */
-    public void setDone(boolean done){
-    	this.done = done;
-    }
-
+    
 	/**
 	 * Returns the startDate field
 	 * @return startDate
